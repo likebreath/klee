e8b138757a7a1e8817203eb81cbef5e1e4f4741d
Bo Chen <chenbo@pdx.edu>
2017-10-08 19:11:30 -0700

    Cleanup deprecated code
    1. Code for m_cpuStateSyncTables
    2. Code for m_memoSyncTables
    3. Code for replay crete_make_concolic()

--- include/klee/ExecutionState.h
+++ include/klee/ExecutionState.h
@@ -200,10 +200,6 @@ public:
   // current tb_pc
   uint64_t crete_current_tb_pc;
 
-  void pushCreteConcolic(ConcolicVariable cv);
-  ConcolicVariable getFirstConcolic();
-  void printCreteConolic();
-
   bool isSymbolics(const MemoryObject *mo);
   ref<Expr> getConcreteExpr(ref<Expr> e);
 
@@ -220,8 +216,6 @@ public:
   void simplifyConstraintsWithConcolicValue();
 
 private:
-  std::deque<ConcolicVariable > creteConcolicsQueue;
-
   // trace tag
   uint64_t m_trace_tag_current_node_index;
 
--- lib/crete-replayer/qemu_rt_info.cpp
+++ lib/crete-replayer/qemu_rt_info.cpp
@@ -24,17 +24,16 @@ QemuRuntimeInfo::QemuRuntimeInfo()
     init_interruptStates();
 
     // not-streamed
-	init_concolics();
+    init_trace_tag();
 
-	CRETE_CK(read_debug_cpuState_offsets(););
+    CRETE_CK(read_debug_cpuState_offsets(););
 }
 
 QemuRuntimeInfo::~QemuRuntimeInfo()
 {
-    cleanup_concolics();
 }
 
-static void check_cpu_state(klee::ExecutionState &state, klee::ObjectState *os_current_cpu_state,
+static void check_cpu_state(klee::ExecutionState &state, const klee::ObjectState *os_current_cpu_state,
                             const vector<CPUStateElement>& correct_cpu_state, uint64_t tb_index)
 {
     bool cross_check_passed = true;
@@ -99,174 +98,16 @@ static void check_cpu_state(klee::ExecutionState &state, klee::ObjectState *os_c
     }
 }
 
-void QemuRuntimeInfo::sync_cpuState(klee::ExecutionState &state, klee::ObjectState *wos, uint64_t tb_index) {
-    if(tb_index >= m_streamed_tb_count) {
-        read_streamed_trace();
-        assert((m_streamed_tb_count - tb_index) == m_cpuStateSyncTables.size());
-    }
-
-    uint64_t adjusted_tb_index = tb_index - (m_streamed_tb_count - m_cpuStateSyncTables.size());
-
-    cpuStateSyncTable_ty cpuStateSyncTable = m_cpuStateSyncTables[adjusted_tb_index];
-
-    CRETE_DBG(
-    cerr << "-------------------------------------------------------\n";
-    cerr << "tb-" << dec << tb_index << ": sync_cpuState()\n";
-    );
-
-    if(!cpuStateSyncTable.first) return;
-
-    assert(!cpuStateSyncTable.second.empty());
-
-    check_cpu_state(state, wos, cpuStateSyncTable.second, tb_index);
-
-    CRETE_DBG(cerr << " concretized elements: \n";);
-    for(vector<CPUStateElement>::const_iterator it = cpuStateSyncTable.second.begin();
-            it != cpuStateSyncTable.second.end(); ++it) {
-        if(it->m_name.find("debug") != string::npos) {
-            continue;
-        }
-
-        assert(it->m_data.size() == it->m_size);
-        wos->write_n(it->m_offset,it->m_data);
-        CRETE_DBG(fprintf(stderr, "(%s:%lu): [", it->m_name.c_str(), it->m_size);
-        for(uint64_t i = 0; i < it->m_size; ++i) {
-            cerr << hex << " 0x" << (uint32_t)it->m_data[i];
-        }
-        cerr << "]\n";
-        );
-    }
-}
-
-const memoSyncTable_ty& QemuRuntimeInfo::get_memoSyncTable(uint64_t tb_index)
-{
-    if(tb_index >= m_streamed_tb_count)
-    {
-        read_streamed_trace();
-        assert((m_streamed_tb_count - tb_index) == m_memoSyncTables.size());
-    }
-
-    uint64_t adjusted_tb_index = tb_index - (m_streamed_tb_count - m_memoSyncTables.size());
-    assert(adjusted_tb_index < m_memoSyncTables.size());
-
-    return m_memoSyncTables[adjusted_tb_index];
-}
-
-void QemuRuntimeInfo::printMemoSyncTable(uint64_t tb_index)
-{
-	memoSyncTable_ty temp_mst = m_memoSyncTables[tb_index];
-
-	cerr << "memoSyncTable content of index " << dec << tb_index << ": ";
-
-	if(temp_mst.empty()){
-		cerr << " NULL\n";
-	} else {
-		cerr << "size = " << temp_mst.size() << ":\n";
-		for(memoSyncTable_ty::iterator m_it = temp_mst.begin();
-				m_it != temp_mst.end(); ++m_it){
-
-			cerr << hex << "0x" << m_it->first << ": (0x " << m_it->second << "); ";
-		}
-
-		cerr << dec << endl;
-	}
-}
-
-concolics_ty QemuRuntimeInfo::get_concolics() const
-{
-	return m_concolics;
-}
-
-void QemuRuntimeInfo::check_file_symbolics()
-{
-    ifstream ifs("dump_mo_symbolics.txt", ios_base::binary);
-    vector<string> symbolics;
-    string symbolic_entry;
-    while(getline(ifs, symbolic_entry, '\n')) {
-    	symbolics.push_back(symbolic_entry);
-    }
-
-    ifs.close();
-
-    set<string> unique_symbolics;
-    vector<string> output_symbolics;
-    for(vector<string>::iterator i = symbolics.begin();
-    		i != symbolics.end(); ++i) {
-    	if(unique_symbolics.insert(*i).second){
-    		output_symbolics.push_back(*i);
-    	}
-    }
-
-    ofstream ofs("dump_mo_symbolics.txt", ios_base::binary);
-    for(vector<string>::iterator i = output_symbolics.begin();
-        		i != output_symbolics.end(); ++i) {
-    	ofs << *i << '\n';
-    }
-    ofs.close();
-}
-
 //Get the information of concolic variables from file "dump_mo_symbolics" and "concrete_inputs.bin"
-void QemuRuntimeInfo::init_concolics()
+void QemuRuntimeInfo::init_trace_tag()
 {
     using namespace crete;
-
-    check_file_symbolics();
 
     ifstream inputs("concrete_inputs.bin", ios_base::in | ios_base::binary);
     assert(inputs && "failed to open concrete_inputs file!");
     const TestCase tc = read_serialized(inputs);
 
     m_base_tc_issue_index = tc.get_issue_index();
-
-    // Get the concrete value of conoclic variables and put them in a map indexed by name
-    vector<TestCaseElement> tc_elements = tc.get_elements();
-    map<string, cv_concrete_ty> map_concrete_value;
-    for(vector<TestCaseElement>::const_iterator it = tc_elements.begin();
-    		it != tc_elements.end(); ++it) {
-    	string c_name(it->name.begin(), it->name.end());
-    	cv_concrete_ty pair_concrete_value(it->data_size,
-    			it->data);
-    	map_concrete_value.insert(pair<string, cv_concrete_ty>(c_name,
-    			pair_concrete_value));
-    }
-    assert(map_concrete_value.size() == tc_elements.size());
-
-    ifstream ifs("dump_mo_symbolics.txt");
-    assert(ifs && "failed to open dump_mo_symbolics file!");
-
-    string name;
-    vector<uint8_t> concrete_value;
-    uint64_t data_size;
-    uint64_t guest_addr;
-    uint64_t host_addr;
-
-    uint64_t name_addr;
-    uint64_t fake_val;
-
-    map<string, cv_concrete_ty>::iterator map_it;
-    ConcolicVariable *cv;
-    string line;
-
-    while(getline(ifs, line)) {
-        stringstream sym_ss(line);
-        sym_ss >> name;
-        sym_ss >> name_addr;
-        sym_ss >> fake_val;
-        sym_ss >> data_size;
-        sym_ss >> guest_addr;
-        sym_ss >> host_addr;
-
-        map_it = map_concrete_value.find(name);
-        assert(map_it != map_concrete_value.end() &&
-        		"concrete value for a concolic variable is not found!\n");
-
-        concrete_value = map_it->second.second;
-        data_size= map_it->second.first;
-
-        cv = new ConcolicVariable(name, concrete_value, data_size,
-        		guest_addr, host_addr);
-        m_concolics.push_back(cv);
-    }
 
     m_trace_tag_explored = tc.get_traceTag_explored_nodes();
     m_trace_tag_semi_explored = tc.get_traceTag_semi_explored_node();
@@ -283,56 +124,6 @@ void QemuRuntimeInfo::init_concolics()
     );
 }
 
-void QemuRuntimeInfo::cleanup_concolics()
-{
-	while(!m_concolics.empty()){
-		ConcolicVariable *ptr_cv = m_concolics.back();
-		m_concolics.pop_back();
-		delete ptr_cv;
-	}
-}
-
-void QemuRuntimeInfo::print_memoSyncTables()
-{
-	uint64_t temp_tb_count = 0;
-	cerr << "[Memo Sync Table]\n";
-	for(memoSyncTables_ty::const_iterator it = m_memoSyncTables.begin();
-			it != m_memoSyncTables.end(); ++it){
-		if(it->empty()){
-			cerr << "tb_count: " << dec << temp_tb_count++ << ": NULL\n";
-		} else {
-			cerr << "tb_count: " << dec << temp_tb_count++
-					<< ", size = " << it->size() << ": ";
-			for(memoSyncTable_ty::const_iterator m_it = it->begin();
-					m_it != it->end(); ++m_it){
-				cerr << hex << "0x" << m_it->first << ": (0x " << (uint64_t)m_it->second  << "); ";
-			}
-
-			cerr << dec << endl;
-		}
-	}
-}
-
-void QemuRuntimeInfo::print_cpuSyncTable(uint64_t tb_index) const
-{
-    if(!m_cpuStateSyncTables[tb_index].first) {
-        cerr << "tb-" << dec << tb_index << ": cpuSyncTable is empty\n";
-    }
-
-    cerr << "tb-" << dec << tb_index << ": cpuSyncTable size = "
-            << m_cpuStateSyncTables[tb_index].second.size() << endl;
-
-    for(vector<CPUStateElement>::const_iterator it = m_cpuStateSyncTables[tb_index].second.begin();
-            it != m_cpuStateSyncTables[tb_index].second.end(); ++it) {
-        cerr << it->m_name << ": " << it->m_size << " bytes"
-                << " [";
-        for(uint64_t i = 0; i < it->m_size; ++i) {
-            cerr << " 0x"<< hex << (uint32_t)it->m_data[i];
-        }
-        cerr << "]\n";
-    }
-}
-
 void QemuRuntimeInfo::init_interruptStates()
 {
     ifstream i_sm("dump_qemu_interrupt_info.bin", ios_base::binary);
@@ -344,32 +135,10 @@ void QemuRuntimeInfo::init_interruptStates()
 
 void QemuRuntimeInfo::read_streamed_trace()
 {
-    uint32_t read_amt_cst = read_cpuSyncTables();
     uint32_t read_amt_dbg_cst = read_debug_cpuSyncTables();
-    uint32_t read_amt_mst = read_memoSyncTables();
 
-    assert(read_amt_cst == read_amt_dbg_cst);
-    assert(read_amt_cst == read_amt_mst);
-
-    m_streamed_tb_count += read_amt_cst;
+    m_streamed_tb_count += read_amt_dbg_cst;
     ++m_streamed_index;
-}
-
-uint32_t QemuRuntimeInfo::read_cpuSyncTables()
-{
-    stringstream ss;
-    ss << "dump_sync_cpu_states." << m_streamed_index << ".bin";
-    ifstream i_sm(ss.str().c_str(), ios_base::binary);
-    if(!i_sm.good()) {
-        cerr << "[Crete Error] can't find file " << ss.str() << endl;
-        assert(0);
-    }
-
-    boost::archive::binary_iarchive ia(i_sm);
-    m_cpuStateSyncTables.clear();
-    ia >> m_cpuStateSyncTables;
-
-    return m_cpuStateSyncTables.size();
 }
 
 uint32_t QemuRuntimeInfo::read_debug_cpuSyncTables()
@@ -392,23 +161,6 @@ uint32_t QemuRuntimeInfo::read_debug_cpuSyncTables()
     });
 
     return m_debug_cpuStateSyncTables.size();
-}
-
-uint32_t QemuRuntimeInfo::read_memoSyncTables()
-{
-    stringstream ss;
-    ss << "dump_new_sync_memos." << m_streamed_index << ".bin";
-    ifstream i_sm(ss.str().c_str(), ios_base::binary);
-    if(!i_sm.good()) {
-        cerr << "[Crete Error] can't find file " << ss.str() << endl;
-        assert(0);
-    }
-
-    boost::archive::binary_iarchive ia(i_sm);
-    m_memoSyncTables.clear();
-    ia >> m_memoSyncTables;
-
-    return m_memoSyncTables.size();
 }
 
 void QemuRuntimeInfo::read_debug_cpuState_offsets()
@@ -445,7 +197,7 @@ static void concretize_incorrect_cpu_element(klee::ObjectState *cpu_os,
 }
 
 void QemuRuntimeInfo::cross_check_cpuState(klee::ExecutionState &state,
-        klee::ObjectState *os, uint64_t tb_index_input) {
+        const klee::ObjectState *os, uint64_t tb_index_input) {
     if(tb_index_input == 0)
         return;
 
