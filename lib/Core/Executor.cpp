18a251f1c35d6acbf8807621af627b525570524a
Bo Chen <chenbo@pdx.edu>
2018-03-14 12:44:31 -0700

    Enable debug prints for VD tests
    1. Enable prints on test generation from VD code
    2. Enable prints with calls into VD code (with symbolic inputs)

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3591,6 +3591,7 @@ void Executor::executeMemoryOperation(ExecutionState &state,
 
       CRETE_DBG(
       fprintf(stderr, "[CRETE Warning] Concretization: symbolic address.\n");
+      state.print_stack();
       );
   }
 #endif // CRETE_CONFIG
@@ -5241,6 +5242,7 @@ void Executor::handleCreteBcPrint(klee::Executor* executor,
         }
         fprintf(stderr, "\"\n");
     }
+    state->print_stack();
 }
 
 void Executor::handleCreteBcAssert(klee::Executor* executor,
